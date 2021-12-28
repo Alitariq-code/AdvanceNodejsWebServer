c0e9ea5d66329717526bd4341dcd617ac4aa6131
Ali_Tariq <82971663+Alitariq-code@users.noreply.github.com>
2021-12-28 14:03:04 +0500

    added pagination,sorting,and selecting

--- controllers/tourControler.js
+++ controllers/tourControler.js
@@ -17,7 +17,36 @@ exports.getAlltour = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3 Sortinggggggg
+    if (req.query.sort) {
+      //Here we are checking multiple values to get sort values
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    // -Value:To get values in other direction
+
+    //Get required data
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(page).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('this page doest exict');
+    }
 
     const tours = await query;
     res.status(200).json({
